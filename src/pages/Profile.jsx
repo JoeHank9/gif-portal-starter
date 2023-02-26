5bff232963cd70101271e406d1fa7b948c433fda
Alezanaab <alezana@outlook.es>
2023-02-26 13:07:58 -0700

    Adding styling to profile view

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -225,6 +225,14 @@ function Profile() {
       instagram: instagram.to_string(),
       youtube: youtube.to_string()
             */}
+            <div className="max-w-3xl mx-auto text-center pb-12 md:pb-16">
+              <h1 className="h1 mb-4" data-aos="fade-up">
+                Perfil
+              </h1>
+              <p className="text-xl text-gray-400 mb-8" data-aos="fade-up" data-aos-delay="200">
+                Rellena el formulario para completar tu perfil
+              </p>
+            </div>
             
           
           <form
